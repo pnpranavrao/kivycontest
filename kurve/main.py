f3d19f6546e1aed02c1ec993c6bd55f1c2f9f767
Pranav Rao <pnpranavrao@gmail.com>
2012-01-30 00:34:03 +0530

    Sexy new algo to check collision detection finally works

--- kurve/main.py
+++ kurve/main.py
@@ -8,26 +8,26 @@ from kivy.uix.widget import Widget
 from kivy.uix.popup import Popup
 from kivy.clock import Clock
 from kivy.properties import ObjectProperty,DictProperty
-from kivy.graphics import Line,Color
+from kivy.graphics import Line,Color,Rectangle
+import math
 
 class KurveApp(App):
     def build(self):
         game = KurveGame()
         x = game.width
         y = game.top
-        leftbtn = Button(text="left",pos=(0,y),size=(x/8,y/5))
+        leftbtn = Button(text="left",pos=(0,y-y/5),size=(x/8,y/5))
         rightbtn = Button(text = "right",pos = (0,0),size=(x/8,y/5))
-        leftbtn2 = Button(text="left",pos = (x,0),size=(x/8,y/5))
-        rightbtn2 = Button(text = "right",pos = (x,y),size=(x/8,y/5))
+        leftbtn2 = Button(text="left",pos = (x-x/8,0),size=(x/8,y/5))
+        rightbtn2 = Button(text = "right",pos = (x-x/8,y-y/5),size=(x/8,y/5))
         game.add_widget(leftbtn)
         game.add_widget(rightbtn)
         game.add_widget(leftbtn2)
         game.add_widget(rightbtn2)
         game.begin()
-        Clock.schedule_interval(game.update,1.0/6.0)
-        Clock.schedule_interval(game.check,.8)
-        
-                     
+        Clock.schedule_interval(game.update,1.0/10.0)
+        #Clock.schedule_interval(game.check,.8)
+                    
         def turn_left(obj):
             game.snake1.velocity = Vector(*game.snake1.velocity).rotate(90)
         def turn_right(obj):
@@ -45,12 +45,13 @@ class KurveApp(App):
         
 class KurveGame(Widget):
     a = dict()
-    a['hello'] = 2 #These two steps are done just so that the dict is properly initialized.
+    a['hello'] = 2 
+#These two steps are done just so that the dict is properly initialized.
     data = DictProperty(a)
-    
-    popup1 = Popup(title = "Winner",content = Label(text='Player1 wins'),size = (300,300))
-    popup2 = Popup(title = "Winner",content = Label(text='Player2 wins'),size = (300,300))
-    
+    '''
+    popup1 = Popup(title = "Winner",content = Label(text='Player1 wins',size =(300,300)))
+    popup2 = Popup(title = "Winner",content = Label(text='Player2 wins',size = (300,300)))
+    ''' 
     snake1 = ObjectProperty(None)
     snake2 = ObjectProperty(None)
     
@@ -60,18 +61,37 @@ class KurveGame(Widget):
     t = Clock.create_trigger(stop,timeout = 2)
     
     def begin(self):
-        self.snake1.pos = self.x,self.y
-        self.data["line1_x"] = [self.snake1.pos[0]]
-        self.data["line1_y"] = [self.snake1.pos[1]]
-        self.data['current1'] = [(self.snake1.pos[0],self.snake1.pos[1]),(self.snake1.pos[0],self.snake1.pos[1])]
+        y=self.right
+        x=self.width
+        with self.canvas:
+            Rectangle(pos=(0,0),size=(x/60,y))
+            Rectangle(pos=(0,0),size=(x,y/60))
+            Rectangle(pos=(x-x/60,0),size=(x/60,y))
+            Rectangle(pos=(0,y-y/60),size=(x,y/60))
+        self.data['div']=(150.0,150.0)
+        self.data['pix']=(600.0,600.0)
+        divx,divy = int(self.data['div'][0]),int(self.data['div'][1])
         
-        self.snake2.pos = self.width,self.top
-        self.data["line2_x"] = [self.snake2.pos[0]]
-        self.data["line2_y"] = [self.snake2.pos[1]]
-        self.data['current2'] = [(self.snake2.pos[0],self.snake2.pos[1]),(self.snake2.pos[0],self.snake2.pos[1])]
-        
-        self.snake1.velocity = (10,0)
-        self.snake2.velocity = (-10,0)
+        self.data['occupied'] = list()
+        for i in range(divx*divy):
+            self.data['occupied'] += [0]
+        for i in range(divx):
+            self.data['occupied'][i]=1
+            self.data['occupied'][-i-1]=1
+        for i in range(divy):
+            self.data['occupied'][i*divx]=1
+        for i in range(1,divy+1):
+            self.data['occupied'][(i*divx)-1]=1
+
+        for i in range(divy):
+            for j in range(divx):
+                print self.data['occupied'][(i*divx)+j],
+            print "\n"
+
+        self.snake1.pos = self.width/6,self.top/6
+        self.snake2.pos = self.width*5/6,self.top*5/6
+        self.snake1.velocity = (7,0)
+        self.snake2.velocity = (-7,0)
         with self.canvas:
             Color(255,255,0)
             self.data["line1"] = Line(points = (self.snake1.center_x,self.snake1.center_y))
@@ -80,60 +100,70 @@ class KurveGame(Widget):
         
     def update(self,dt):
         self.snake1.move1(self.data)
-        self.snake1.current1(self.data)
         self.snake2.move2(self.data)
-        self.snake2.current2(self.data)
+    '''              
+                
+                
         
-    def check(self,dt):
-        val = self.data
-        cur1 = self.data['current2'][0]
-        cur2 = self.data['current2'][1]
-        x1 = max(cur1[0],cur2[0])
-        x2 = min(cur1[0],cur2[0])
-        y1 = max(cur1[1],cur2[1])
-        y2 = min(cur1[1],cur2[1])
-        v = 10
-        for i in range(len(val['line1_x'])):
-            if (val['line1_x'][i] > x2-v)&(val['line1_x'][i] < x1+v)&(val['line1_y'][i] > y2-v)&(val['line1_y'][i] < y1+v):
-                print "Player 2 loses"
-                self.popup1.open()
-                self.t()
-                
-                
-                
-        cur1 = self.data['current1'][0]
-        cur2 = self.data['current1'][1]
-        x1 = max(cur1[0],cur2[0])
-        x2 = min(cur1[0],cur2[0])
-        y1 = max(cur1[1],cur2[1])
-        y2 = min(cur1[1],cur2[1])
-        for i in range(len(val['line2_x'])):
-            if (val['line2_x'][i] > x2-v)&(val['line2_x'][i] < x1+v)&(val['line2_y'][i] > y2-v)&(val['line2_y'][i] < y1+v):
                 print "Player 1 loses"
                 self.popup2.open()
                 self.t()        
-         
+    '''     
         
 class Snake(Widget):
     def move1(self,data):
         self.pos = Vector(*self.velocity)+self.pos
-        data["line1_x"] += [self.pos[0]]
-        data["line1_y"] += [self.pos[1]]
         data["line1"].points += (self.center_x,self.center_y)
-    def current1(self,data):
-        data['current1'][1] = data['current1'][0]
-        data['current1'][0] = (self.pos[0],self.pos[1])
-        
-    def current2(self,data):
-        data['current2'][1] = data['current2'][0]
-        data['current2'][0] = (self.pos[0],self.pos[1])
+        n = self.convert(data)
+        print n
+        data['occupied'][n] = 1
+        self.check(data)
         
     def move2(self,data):
         self.pos = Vector(*self.velocity)+self.pos
-        data["line2_x"] += [self.pos[0]]
-        data["line2_y"] += [self.pos[1]] 
-        #print data['line2_x']
         data["line2"].points += (self.center_x,self.center_y)
+        n = self.convert(data)
+        data['occupied'][n] = 1
+        print "    ",n
+        self.check(data)
+        
+    def convert(self,data):
+        x_pix,y_pix = data['pix']
+        divx,divy = data['div']
+        x,y = self.pos
+        x = math.floor(float(x)/float(x_pix/divx))
+        y = math.floor(float(y)/float(y_pix/divy))
+        n = (divx-1)*y + x
+        if n<divx*divy:
+            return int(n)
+        else:
+            print "large value",n,self.pos,x,y
+            return 300
+    
+    def convert1(self,pos,data):
+        x_pix,y_pix = data['pix']
+        divx,divy = data['div']
+        x,y = pos
+        x = math.ceil(float(x)/float(x_pix/divx))
+        y = math.floor(float(y)/float(y_pix/divy))
+        n = (divx)*y + x
+        if n<divx*divy:
+            print "check at",int(n)
+            print "----------"
+            return int(n)
+        else:
+            print "large value",n,self.pos,x,y
+            return 300
+       
+    def check(self,data):
+        pos = Vector(*self.velocity)+self.pos
+        if data['occupied'][self.convert1(pos,data)]==1:
+            print "Died at",pos,self.convert1(pos,data)
+            print "game over"
+            exit(1)
+            
+        
+        
 
     
         
